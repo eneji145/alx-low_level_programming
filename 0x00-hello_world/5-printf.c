9a2ec2b5d9cf03a387fdfcfaf90ff518fae349d0
eneji145 <patmuffs@gmail.com>
2022-09-08 16:15:59 -0700

    printing in c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,6 +3,8 @@
  * main prints the string in the main function
  * the output of the program is to print with proper grammar, but the outcome is a piece of art,
  * */
-int main (void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);}
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
