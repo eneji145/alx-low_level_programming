3b63ee370af5ea4ea38df2c7bd2f92e3ba9d179c
eneji145 <patmuffs@gmail.com>
2022-09-08 15:48:25 -0700

    finally coding in c from the terminal

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- put the string in the function named puts
+ * Description: This program prints Programming is like building a multilingual puzzle, followed by a new line and retuns nothing 
+ */
+int main (void)
+{
+	puts("\"programming is  like building a multilingual puzzle");
+	return (0);
+}
