4e8ddad1f58d05f9b631a71e255efd1596dc52da
eneji145 <patmuffs@gmail.com>
2022-09-08 17:01:51 -0700

    datatypes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %1d byte(s)\n", sizeof(c));
 	printf("Size of an int: %1d byte(s)\n", sizeof(i));
-        printf("Size of a long int: %1d byte(s)\n", sizeof(li));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(li));
 	printf("Size of long long int: %1d byte(s)\n", sizeof(lli));
-	printf("Size of a float: %1d byte(s)\n", sizeof(f));
-	return(0);
-}	
+	printf ("Size of a float: %1d byte(s)\n", sizeof(f));
+	return (0);
+}
